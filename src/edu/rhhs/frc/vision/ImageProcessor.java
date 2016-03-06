1cc52a094be59e0868abf8495258804b0a59d9cf
BrianSelle <bselle@cebridge.net>
2016-03-06 13:56:34 -0600

    add camera code from .NET app

--- src/edu/rhhs/frc/vision/ImageProcessor.java
+++ src/edu/rhhs/frc/vision/ImageProcessor.java
@@ -1,14 +1,13 @@
 package edu.rhhs.frc.vision;
 
+import java.text.DecimalFormat;
+
 import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.DrawMode;
 import com.ni.vision.NIVision.Image;
 import com.ni.vision.NIVision.ImageType;
-import com.ni.vision.NIVision.ParticleFilterCriteria2;
-
-import edu.wpi.first.wpilibj.image.BinaryImage;
-import edu.wpi.first.wpilibj.image.ColorImage;
-import edu.wpi.first.wpilibj.image.NIVisionException;
-import edu.wpi.first.wpilibj.image.ParticleAnalysisReport;
+import com.ni.vision.NIVision.RGBValue;
+import com.ni.vision.NIVision.ShapeMode;
 
 /**
  * @author rhhs
@@ -18,19 +17,11 @@ import edu.wpi.first.wpilibj.image.ParticleAnalysisReport;
  */
 public class ImageProcessor {
     
-    private static final int NUM_SMALL_OBJECT_EROSIONS = 2;
+    private static final int NUM_SMALL_OBJECT_EROSIONS = 1;
     
-    private static final int THRESHOLD_HUE_MIN = 82;
-    private static final int THRESHOLD_HUE_MAX = 137;
-    private static final int THRESHOLD_SATURATION_MIN = 35;
-    private static final int THRESHOLD_SATURATION_MAX = 255;
-    private static final int THRESHOLD_LUMINANCE_MIN = 60;
-    private static final int THRESHOLD_LUMINANCE_MAX = 255;
-    
-    private static final int PARTICLE_SEARCH_HEIGHT_MIN = 20;
-    private static final int PARTICLE_SEARCH_HEIGHT_MAX = 400;
-    private static final int PARTICLE_SEARCH_WIDTH_MIN = 20;
-    private static final int PARTICLE_SEARCH_WIDTH_MAX = 400;
+    private NIVision.Range TARGET_HUE_RANGE = new NIVision.Range(82, 137);	
+    private NIVision.Range TARGET_SAT_RANGE = new NIVision.Range(35, 255);	
+    private NIVision.Range TARGET_LUM_RANGE = new NIVision.Range(60, 255);	
     
     public static final double TARGET_HEIGHT_FT = 1.0;
     public static final double TARGET_WIDTH_FT = 20.0/12.0;
@@ -45,101 +36,108 @@ public class ImageProcessor {
     public static final double WEIGHT_FACTOR_XX = 10;
     public static final double WEIGHT_FACTOR_YY = 10;
     
-    public static final double CAMERA_FOV_HORIZONTAL_ANGLE = 48.0;  // M206 54 deg actual
-    public static final double CAMERA_FOV_VERTICAL_ANGLE = 36.9;  // M206 41.8 deg actual
+    public static final double CAMERA_FOV_HORIZONTAL_ANGLE = 33.565;  
+    public static final double CAMERA_FOV_VERTICAL_ANGLE = 59.695;  
+	public static final double tanHalfFOV = Math.tan(Math.toRadians(CAMERA_FOV_HORIZONTAL_ANGLE / 2));
         
+    private Image processedImage;
+
     public ImageProcessor() {
-    	
+        processedImage = NIVision.imaqCreateImage(ImageType.IMAGE_U8, 0);
     }
 
-    public TargetInfo findBestTarget(Image cameraImage, String outputFilename) { 
-    	ParticleAnalysisReport[] reports = identifyRectangularTargets(cameraImage, outputFilename);
-    	
-    	ParticleAnalysisReport bestTarget = null;
-    	double bestCompositeScore = 10000;
-        for (ParticleAnalysisReport currentReport: reports) { 
-        	double currentCompositeScore = getCompositeScore(currentReport);
-            if (currentCompositeScore < bestCompositeScore) {
-            	bestCompositeScore = currentCompositeScore;
-            	bestTarget = currentReport;
-            }
-        }
-        
-        return new TargetInfo(bestTarget, bestCompositeScore);
-    }
- 
-    
-    private ParticleAnalysisReport[] identifyRectangularTargets(Image cameraImage, String outputFilename) {
+    public TargetInfo findBestTarget(Image cameraImage) { 
         if (cameraImage == null) {
             return null;
         }
 
-        Image thresholdImage = NIVision.imaqCreateImage(ImageType.IMAGE_U8, 0);
-        Image bigObjectsImage = NIVision.imaqCreateImage(ImageType.IMAGE_U8, 0);
-        ParticleAnalysisReport[] reports = null;
-        try {
-//        	NIVision.imaqColorThreshold(thresholdImage, cameraImage, THRESHOLD_HUE_MIN, THRESHOLD_HUE_MAX, THRESHOLD_SATURATION_MIN, THRESHOLD_SATURATION_MAX, THRESHOLD_LUMINANCE_MIN, THRESHOLD_LUMINANCE_MAX);
-            if (thresholdImage == null) {
-                return null;
-            }
-            
-//            bigObjectsImage = thresholdImage.removeSmallObjects(true, NUM_SMALL_OBJECT_EROSIONS);
-            if (bigObjectsImage == null) {
-                return null;
-            }
-            
-            // Set up the particle criteria
-            ParticleFilterCriteria2[] particleSearchCriteriaCollection = new ParticleFilterCriteria2[2];
-            particleSearchCriteriaCollection[0] = new ParticleFilterCriteria2(NIVision.MeasurementType.MT_BOUNDING_RECT_WIDTH, PARTICLE_SEARCH_WIDTH_MIN, PARTICLE_SEARCH_WIDTH_MAX, 0, 0);
-            particleSearchCriteriaCollection[1] = new ParticleFilterCriteria2(NIVision.MeasurementType.MT_BOUNDING_RECT_HEIGHT, PARTICLE_SEARCH_HEIGHT_MIN, PARTICLE_SEARCH_HEIGHT_MAX, 0, 0);
-//            bigObjectsImage.particleFilter(particleSearchCriteriaCollection);
-
-            // Get the particle reports
-//            reports = bigObjectsImage.getOrderedParticleAnalysisReports();
-           
-            if (outputFilename != null) {
-//            	bigObjectsImage.write(outputFilename);
-//                System.out.println("Processed Image Saved = " + outputFilename + ", size = " + bigObjectsImage.getHeight() + "X" + bigObjectsImage.getWidth());
-            }
-        } 
-//        catch (NIVisionException e) {
-//            System.err.println("NIVision error = " + e.getMessage());            
-//        } 
-        finally {
-//            try {
-//                if (thresholdImage != null) {
-//                    thresholdImage.free();
-//                }
-//                if (bigObjectsImage != null) {
-//                    bigObjectsImage.free();
-//                }
-//            }
-//            catch (NIVisionException e) {
-//                System.err.println("Error freeing image = " + e.getMessage());            
-//            }
+    	NIVision.imaqColorThreshold(processedImage, cameraImage, 1, NIVision.ColorMode.HSL, TARGET_HUE_RANGE, TARGET_SAT_RANGE, TARGET_LUM_RANGE);
+        if (processedImage == null) {
+            return null;
         }
+        
+        NIVision.imaqSizeFilter(processedImage, processedImage, 1, NUM_SMALL_OBJECT_EROSIONS, NIVision.SizeType.KEEP_LARGE, null);
+        if (processedImage == null) {
+            return null;
+        }
+        
+        // The measurements are flipped because the image is rotated 90 degrees
+    	int bestTargetIndex = -1;
+    	double bestCompositeScore = 0;
+		int numParticles = NIVision.imaqCountParticles(processedImage, 1);
 
-        return reports;
+    	for (int particleIndex = 0; particleIndex < numParticles; particleIndex++) {     
+			double rectWidth = NIVision.imaqMeasureParticle(processedImage, particleIndex, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_HEIGHT);
+			double rectHeight = NIVision.imaqMeasureParticle(processedImage, particleIndex, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_WIDTH);
+			double rectArea = NIVision.imaqMeasureParticle(processedImage, particleIndex, 0, NIVision.MeasurementType.MT_AREA);
+			double rectMomentYY = NIVision.imaqMeasureParticle(processedImage, particleIndex, 0, NIVision.MeasurementType.MT_NORM_MOMENT_OF_INERTIA_XX);
+			double rectMomentXX = NIVision.imaqMeasureParticle(processedImage, particleIndex, 0, NIVision.MeasurementType.MT_NORM_MOMENT_OF_INERTIA_YY);
+
+			double currentCompositeScore = getCompositeScore(rectArea, rectWidth, rectHeight, rectMomentXX, rectMomentYY);
+            if (bestTargetIndex == -1 || currentCompositeScore < bestCompositeScore) {
+            	bestCompositeScore = currentCompositeScore;
+            	bestTargetIndex = particleIndex;
+            }
+		}
+        
+		double rectTop = NIVision.imaqMeasureParticle(processedImage, bestTargetIndex, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_LEFT);
+		double rectLeft = NIVision.imaqMeasureParticle(processedImage, bestTargetIndex, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_TOP);
+		double rectWidth = NIVision.imaqMeasureParticle(processedImage, bestTargetIndex, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_HEIGHT);
+		double rectHeight = NIVision.imaqMeasureParticle(processedImage, bestTargetIndex, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_WIDTH);
+		NIVision.GetImageSizeResult imageSize;
+		imageSize = NIVision.imaqGetImageSize(processedImage);
+		int imageHeight = imageSize.width;
+		int imageWidth = imageSize.height;
+		
+		// Calculate distance and angle to target
+        int widthOffsetPixels = (int)((rectLeft + rectWidth / 2) - imageWidth / 2);
+               
+    	// Account for targets off center
+        double focalDistancePixels = (double)imageWidth / 2 / tanHalfFOV;
+        double offsetAngle = Math.atan((double)widthOffsetPixels / focalDistancePixels);
+
+        double imageWidthFt = Math.cos(offsetAngle) * (double)imageWidth * TARGET_WIDTH_FT / rectWidth;
+        double cameraDistanceWidthFt = imageWidthFt / 2.0 / tanHalfFOV;
+
+        // Calculate the angle from the center of the image to the selected target
+        double targetOffsetFt = imageWidthFt * widthOffsetPixels / (double)imageWidth;
+        double angleToTargetDeg = Math.atan2(targetOffsetFt, cameraDistanceWidthFt) * 180.0 / Math.PI;
+
+        NIVision.Rect rect = new NIVision.Rect((int)rectLeft, (int)rectTop, (int)rectWidth, (int)rectHeight);
+        NIVision.imaqDrawShapeOnImage(cameraImage, cameraImage, rect, DrawMode.DRAW_VALUE, ShapeMode.SHAPE_RECT, 255.0f);
+        
+        int targetYCoord = (int)(widthOffsetPixels + rectLeft + rectWidth / 2);
+        NIVision.Point startPoint = new NIVision.Point(0, targetYCoord);
+        NIVision.Point endPoint = new NIVision.Point(imageSize.width, targetYCoord);
+        NIVision.imaqDrawLineOnImage(cameraImage, cameraImage, DrawMode.DRAW_VALUE, startPoint, endPoint, 255.0f);
+        
+        NIVision.OverlayTextOptions options = new NIVision.OverlayTextOptions();
+        NIVision.RGBValue textColor = new RGBValue(0, 255, 0, 0);        
+        NIVision.Point textPoint = new NIVision.Point((int)(rectTop + rectHeight/2), (int)targetYCoord);        
+        DecimalFormat numFormat = new DecimalFormat("##.##");
+        NIVision.imaqOverlayText(cameraImage, textPoint, "A = " + numFormat.format(angleToTargetDeg), textColor, options, "");
+        
+        return new TargetInfo(cameraDistanceWidthFt, angleToTargetDeg, bestCompositeScore);
     }
-    
-    private double getRectangleScore(ParticleAnalysisReport report) {
-        return (report.particleArea / ((double)report.boundingRectWidth * (double)report.boundingRectHeight) - OPTIMAL_RECT) * WEIGHT_FACTOR_RECT;
+        
+    private double getRectangleScore(double area, double width, double height) {
+        return (area / (width * height) - OPTIMAL_RECT) * WEIGHT_FACTOR_RECT;
     } 
     
-    private double getXXRatioScore(ParticleAnalysisReport report) {
-        return (report.center_mass_x_normalized - OPTIMAL_XX) * WEIGHT_FACTOR_XX;
+    private double getXXRatioScore(double momentXX) {
+        return (momentXX - OPTIMAL_XX) * WEIGHT_FACTOR_XX;
     }
     
-    private double getYYRatioScore(ParticleAnalysisReport report) {
-        return (report.center_mass_x_normalized - OPTIMAL_YY) * WEIGHT_FACTOR_YY;
+    private double getYYRatioScore(double momentYY) {
+        return (momentYY - OPTIMAL_YY) * WEIGHT_FACTOR_YY;
     }
     
-    private double getAspectRatioScore(ParticleAnalysisReport report) {
-        return (report.boundingRectHeight / (double)report.boundingRectWidth - OPTIMAL_AR) * WEIGHT_FACTOR_AR;
+    private double getAspectRatioScore(double width, double height) {
+        return (height / width - OPTIMAL_AR) * WEIGHT_FACTOR_AR;
     }
     
-    private double getCompositeScore(ParticleAnalysisReport report) {
-        return Math.abs(getRectangleScore(report)) + Math.abs(getXXRatioScore(report)) + Math.abs(getYYRatioScore(report)) + Math.abs(getAspectRatioScore(report));
+    private double getCompositeScore(double area, double width, double height, double momentXX, double momentYY) {
+        return Math.abs(getRectangleScore(area, width, height)) + Math.abs(getXXRatioScore(momentXX)) + Math.abs(getYYRatioScore(momentYY)) + Math.abs(getAspectRatioScore(width, height));
     }
     
     public class TargetInfo {
@@ -147,28 +145,10 @@ public class ImageProcessor {
     	public double angleToTargetDeg;
     	public double compositeScore;
     	
-    	public TargetInfo(ParticleAnalysisReport report, double compositeScore) {
+    	public TargetInfo(double distanceToTargetFt, double angleToTargetDeg, double compositeScore) {
+    		this.distanceToTargetFt = distanceToTargetFt;
+    		this.angleToTargetDeg = angleToTargetDeg;
     		this.compositeScore = compositeScore;
-    		updateCalculations(report);
     	}
-    	
-        private void updateCalculations(ParticleAnalysisReport report) {
-            double tanHalfFOV = Math.tan(Math.toRadians(CAMERA_FOV_HORIZONTAL_ANGLE / 2));
-
-            // Account for targets off center
-            double focalDistancePixels = (double)report.imageWidth / 2 / tanHalfFOV;
-            double targetCenterPixels = (double)report.boundingRectLeft + (double)report.boundingRectWidth / 2;
-            double targetOffsetPixels =  targetCenterPixels - (double)report.imageWidth / 2;
-            double offsetAngle = Math.atan(targetOffsetPixels / focalDistancePixels);
-
-            // Calculate the distance from the camera focal point to the target
-            double imageWidthFt = Math.cos(offsetAngle) * (double)report.imageWidth * TARGET_WIDTH_FT / (double)report.boundingRectWidth;
-            distanceToTargetFt = imageWidthFt / 2.0 / tanHalfFOV;
-
-            // Calculate the angle from the center of the image to the selected target
-            double targetOffsetFt = imageWidthFt * targetOffsetPixels / (double)report.imageWidth;
-            angleToTargetDeg = Math.atan2(targetOffsetFt, distanceToTargetFt) * 180.0 / Math.PI;
-        }   
     }
-
 }
